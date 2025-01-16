06b397734602597829bf3590ba7f915ffc0ad81e
Billu <acharyaanshuman04@gmail.com>
2025-01-16 16:34:02 +0530

    Removed ( commented out ) the socket server and now handling only frontend for rtmp streaming. Now I am using videojs library, but it doesn't directly support rtmp streaming. So, that's to be improved.

--- src/SocketIO/socket.ts
+++ src/SocketIO/socket.ts
@@ -1,122 +1,132 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import http from 'http'
-import express from 'express'
-import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
-import path from 'path'
-import { Server as SocketIO } from 'socket.io'
-import API_BASE_URL from '../constants'
-import axios from 'axios'
-// import { useSelector } from 'react-redux'
-// import { RootState } from '@reduxjs/toolkit/query'
-// import { DeviceState } from '../types'
-// import { error } from 'console'
-// import { data } from 'react-router-dom'
-// import { Stream } from 'stream'
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import http from 'http'
+// import express from 'express'
+// import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
+// import path from 'path'
+// import { Server as SocketIO } from 'socket.io'
+// import API_BASE_URL from '../constants'
+// import axios from 'axios'
+// // import cors from 'cors'
+// // import { useSelector } from 'react-redux'
+// // import { RootState } from '@reduxjs/toolkit/query'
+// // import { DeviceState } from '../types'
+// // import { error } from 'console'
+// // import { data } from 'react-router-dom'
+// // import { Stream } from 'stream'
 
-const app  = express()
-const server = http.createServer(app)
-const io = new SocketIO(server);
-let pbToken = "";
-let pbUrl = "";
-let expirationTime = 0;
+// const app  = express()
+// const server = http.createServer(app)
+// const io = new SocketIO(server);
+// // const corsOptions = {
+// //     origin: 'http://localhost:5000',
+// //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// //     allowedHeaders: ['Content-Type', 'Authorization'],
+// // }
+// let pbToken = "";
+// let pbUrl = "";
+// let expirationTime = 0;
 
-// So, now we have to call api for getting the playback token.
-const getPBToken = async (deviceId: string) => {
-    try {
-        const endPoint = `${API_BASE_URL}/v1/auth/playbackToken`;
-        const response = await axios.post(endPoint, {
-            "deviceId" : "a65561fb-138a-4da0-a6f4-b77e0d367973"
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrbGVhcnZpc2lvbi5jb20iLCJ1c2VyIjp7ImlkIjoiNDZhYzFhMGYtMmY5OC00MTIyLWFkZTktZDAwMDRiNzIzZWYyIiwibmFtZSI6ImFicmFyQGdtYWlsLmNvbSIsInBlcm1pc3Npb25zIjpbImRldmljZS1yZWdpc3RlciIsInN0cmVhbS1wbGF5YmFjayJdfSwiYXV0aGVudGljYXRpb25UeXBlIjoiUEFTU1dPUkQiLCJpYXQiOjE3MzY5NjIwNTEsImV4cCI6MTczNjk2NTY1MX0.Xciehrhc6sXLq9BMxC8aY7Ty-5IJCV-F9Ihcd3WZdRg'
-            }
-        })
+// // So, now we have to call api for getting the playback token.
+// const getPBToken = async (deviceId: string) => {
+//     try {
+//         const endPoint = `${API_BASE_URL}/v1/auth/playbackToken`;
+//         const response = await axios.post(endPoint, {
+//             "deviceId" : "a65561fb-138a-4da0-a6f4-b77e0d367973"
+//         }, {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrbGVhcnZpc2lvbi5jb20iLCJ1c2VyIjp7ImlkIjoiNDZhYzFhMGYtMmY5OC00MTIyLWFkZTktZDAwMDRiNzIzZWYyIiwibmFtZSI6ImFicmFyQGdtYWlsLmNvbSIsInBlcm1pc3Npb25zIjpbImRldmljZS1yZWdpc3RlciIsInN0cmVhbS1wbGF5YmFjayJdfSwiYXV0aGVudGljYXRpb25UeXBlIjoiUEFTU1dPUkQiLCJpYXQiOjE3MzY5NjIwNTEsImV4cCI6MTczNjk2NTY1MX0.Xciehrhc6sXLq9BMxC8aY7Ty-5IJCV-F9Ihcd3WZdRg'
+//             }
+//         })
 
-        if(response) {
-            pbUrl = response.data.playBackUrl
-            pbToken = response.data.playbackAccessToken
-            expirationTime = response.data.expiration
-            console.log(`${pbUrl}?token=${pbToken}`)
-        }
+//         if(response) {
+//             pbUrl = response.data.playBackUrl
+//             pbToken = response.data.playbackAccessToken
+//             expirationTime = response.data.expiration
+//             console.log(`${pbUrl}?token=${pbToken}`)
+//         }
 
-    } catch (error) {
-        console.error("Error fetching playback token: ", error)
-    }
-} 
+//     } catch (error) {
+//         console.error("Error fetching playback token: ", error)
+//     }
+// } 
 
-getPBToken("f8296422-735d-40c3-8616-36b02270a522")
+// getPBToken("f8296422-735d-40c3-8616-36b02270a522")
 
-const options: string[] = [
-    '-i',
-    '-',
-    '-vf', 'scale=1280:720',
-    'aspect', '16:9',
-    '-c:v', 'libx264',
-    '-preset', 'ultrafast',
-    '-tune', 'zerolatency',
-    '-r', `<input-frame-rate>`,                       // ${25}
-    '-g', `${25 * 2}`,
-    '-keyint_min', '25',
-    '-crf', '25',
-    '-pix_fmt', 'yuv420p',                            // This attribute to be checked to ensure compatibility
-    '-sc_threshold', '0',
-    '-profile:v', 'main',
-    '-level', '3.1',
-    '-c:a', 'aac',
-    '-b:a', '128k',
-    '-ar', '32000',
-    '-f', 'flv',
-    `rtmp://VideoProcessingServerASG-NLB-49b63e78c9aa7744.elb.ap-south-1.amazonaws.com:443/live/sk_53RKgM8XBON1ZxHvtDifbd_9vXJVnl5n4LUcZ3JGTGgh7va0bLdFf`,
-];
-// We are getting an error on running straightaway. Docker was needed for running without installing 
-// ffmpeg in our system. However, with proper installation of ffmpeg and addition of it as an environment 
-// variable, docker is no longer required.
+// // app.use(cors(corsOptions))
 
-const ffmpegProcess: ChildProcessWithoutNullStreams = spawn('ffmpeg', options);
+// const options: string[] = [
+//     '-i',
+//     '-',
+//     '-vf', 'scale=1280:720',
+//     'aspect', '16:9',
+//     '-c:v', 'libx264',
+//     '-preset', 'ultrafast',
+//     '-tune', 'zerolatency',
+//     '-r', `<input-frame-rate>`,                       // ${25}
+//     '-g', `${25 * 2}`,
+//     '-keyint_min', '25',
+//     '-crf', '25',
+//     '-pix_fmt', 'yuv420p',                            // This attribute to be checked to ensure compatibility
+//     '-sc_threshold', '0',
+//     '-profile:v', 'main',
+//     '-level', '3.1',
+//     '-c:a', 'aac',
+//     '-b:a', '128k',
+//     '-ar', '32000',
+//     '-f', 'flv',
+//     `rtmp://VideoProcessingServerASG-NLB-49b63e78c9aa7744.elb.ap-south-1.amazonaws.com:443/live/sk_53RKgM8XBON1ZxHvtDifbd_9vXJVnl5n4LUcZ3JGTGgh7va0bLdFf`,
+// ];
+// // We are getting an error on running straightaway. Docker was needed for running without installing 
+// // ffmpeg in our system. However, with proper installation of ffmpeg and addition of it as an environment 
+// // variable, docker is no longer required.
 
-// We are basically adding two event listeners on output and error we have got
-ffmpegProcess.stdout.on('data', (data) => {
-    console.log(`ffmpeg stdout: ${data}`)
-    console.log(`Here at stdout`)
-})
+// const ffmpegProcess: ChildProcessWithoutNullStreams = spawn('ffmpeg', options);
 
-ffmpegProcess.stderr.on('data', (data) => {
-    console.error(`ffmpeg stderr: ${data}`)
-    console.log(`Here at stderr`)
-})
+// // We are basically adding two event listeners on output and error we have got
+// ffmpegProcess.stdout.on('data', (data) => {
+//     console.log(`ffmpeg stdout: ${data}`)
+//     console.log(`Here at stdout`)
+// })
 
-ffmpegProcess.on('close', (code) => {
-    console.log(`ffmpeg process exited with code ${code}`)
-    if(code != 0) {
-        console.error('ffmpeg exited unexpectedly')
-    }
-})
+// ffmpegProcess.stderr.on('data', (data) => {
+//     console.error(`ffmpeg stderr: ${data}`)
+//     console.log(`Here at stderr`)
+// })
 
-ffmpegProcess.on('error', (err) => {
-    console.error(`ffmpeg process error ${err}`)
-})
+// ffmpegProcess.on('close', (code) => {
+//     console.log(`ffmpeg process exited with code ${code}`)
+//     if(code != 0) {
+//         console.error('ffmpeg exited unexpectedly')
+//     }
+// })
 
-app.use(express.static(path.resolve('./public')))
+// ffmpegProcess.on('error', (err) => {
+//     console.error(`ffmpeg process error ${err}`)
+// })
 
-io.on('connection', socket => {
-    console.log("Socket connected", socket.id)
-    socket.on('binarystream', streamData => {
-        // const buffer = Buffer.from(new streamData)
-        console.log('Binary string incoming...')
-        if(ffmpegProcess.stdin.writable) {
-            ffmpegProcess.stdin.write(streamData, (err) => {
-                console.log('Err: ', err)
-            })
-        } else {
-            console.error('ffmpeg stdin is not writable')
-        }
-    })
+// app.use(express.static(path.resolve('./public')))
 
-    socket.on('disconnect', () => {
-        if(!ffmpegProcess.stdin.destroyed) ffmpegProcess.stdin.end()
-        console.log(`Socket disconnected ${socket.id}`)
-    })
-})
+// io.on('connection', socket => {
+//     console.log("Socket connected", socket.id)
+//     socket.on('binarystream', streamData => {
+//         // const buffer = Buffer.from(new streamData)
+//         console.log('Binary string incoming...')
+//         if(ffmpegProcess.stdin.writable) {
+//             ffmpegProcess.stdin.write(streamData, (err) => {
+//                 console.log('Err: ', err)
+//             })
+//         } else {
+//             console.error('ffmpeg stdin is not writable')
+//         }
+//     })
 
-server.listen(5173, () => console.log(`HTTP Server is running on port 5173`))
+//     socket.on('disconnect', () => {
+//         if(!ffmpegProcess.stdin.destroyed) ffmpegProcess.stdin.end()
+//         console.log(`Socket disconnected ${socket.id}`)
+//     })
+// })
+
+// // app.get('example)
+
+// server.listen(5000, () => console.log(`HTTP Server is running on port 5173`))
--- src/components/RTMPStreamer/Streamer.tsx
+++ src/components/RTMPStreamer/Streamer.tsx
@@ -1,26 +1,36 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { FC, useEffect, useRef, useState } from 'react';
-import io, { Socket } from 'socket.io-client';
+import React, { useEffect, useRef, useState } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
-const Streamer: FC = () => {
-    const userVideoRef = useRef<HTMLVideoElement>(null)
+const Streamer: React.FC = () => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
     const [mediaStream, setMediaStream] = useState<MediaStream | null> (null)
-    const [socket, setSocket] = useState<Socket | null> (null)
 
     useEffect(() => {
-        const socketInstance = io('http://localhost:5000')
-        setSocket(socketInstance)
+        if(videoRef.current) {
+            const player = videojs(videoRef.current, {
+                autoplay: true,
+                controls: true,
+                sources: [
+                    {
+                        src: 'rtmp://VideoProcessingServerASG-NLB-49b63e78c9aa7744.elb.ap-south-1.amazonaws.com:443/live/sk_53RKgM8XBON1ZxHvtDifbd_9vXJVnl5n4LUcZ3JGTGgh7va0bLdFf',
+                        type: 'rtmp/flv',
+                    },
+                ],
+            });
 
-        return () => {
-            socketInstance.disconnect()
+            return () => {
+                player.dispose();
+            }
         }
-    }, [])
+    }, []);
 
     const handleStartStreaming = () => {
         if(!mediaStream) {
-            console.error('No media stream available')
-            return;
+            console.error("No media stream available")
+            return
         }
 
         const mediaRecorder = new MediaRecorder(mediaStream, {
@@ -30,13 +40,10 @@ const Streamer: FC = () => {
 
         mediaRecorder.ondataavailable = (e: BlobEvent) => {
             console.log('Binary stream available', e.data)
-            if(socket) {
-                socket.emit('binarystream', e.data)
-            }
         }
 
         mediaRecorder.start(25)
-    };
+    }
 
     useEffect(() => {
         const initializeMedia = async () => {
@@ -46,8 +53,8 @@ const Streamer: FC = () => {
                     video: true
                 })
                 setMediaStream(media)
-                if(userVideoRef.current) {
-                    userVideoRef.current.srcObject = media;
+                if(videoRef.current) {
+                    videoRef.current.srcObject = media;
                 }
             } catch (error) {
                 console.log('Error accessing media devices', error)
@@ -63,7 +70,7 @@ const Streamer: FC = () => {
                 <h1 className='text-3xl font-bold mb-3 text-gray-900'>Live Video Streaming</h1>
                 <div className='relative w-1/4 max-w-3xl aspect-w-16 aspect-h-9 mb-4'>
                     <video 
-                        ref={userVideoRef}
+                        ref={videoRef}
                         autoPlay
                         muted
                         playsInline
@@ -82,3 +89,132 @@ const Streamer: FC = () => {
 }
 
 export default Streamer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /* eslint-disable react-refresh/only-export-components */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import React, { FC, useEffect, useRef, useState } from 'react';
+// import io, { Socket } from 'socket.io-client';
+
+
+// const Streamer: FC = () => {
+//     const userVideoRef = useRef<HTMLVideoElement>(null)
+//     const [mediaStream, setMediaStream] = useState<MediaStream | null> (null)
+//     const [socket, setSocket] = useState<Socket | null> (null)
+
+//     useEffect(() => {
+//         const socketInstance = io('http://localhost:5000')
+//         setSocket(socketInstance)
+
+//         return () => {
+//             socketInstance.disconnect()
+//         }
+//     }, [])
+
+//     const handleStartStreaming = () => {
+//         if(!mediaStream) {
+//             console.error('No media stream available')
+//             return;
+//         }
+
+        // const mediaRecorder = new MediaRecorder(mediaStream, {
+        //     audioBitsPerSecond: 128000,
+        //     videoBitsPerSecond: 2500000,
+        // })
+
+        // mediaRecorder.ondataavailable = (e: BlobEvent) => {
+        //     console.log('Binary stream available', e.data)
+        //     if(socket) {
+        //         socket.emit('binarystream', e.data)
+        //     }
+        // }
+
+//         mediaRecorder.start(25)
+//     };
+
+    // useEffect(() => {
+    //     const initializeMedia = async () => {
+    //         try {
+    //             const media = await navigator.mediaDevices.getUserMedia({
+    //                 audio: true,
+    //                 video: true
+    //             })
+    //             setMediaStream(media)
+    //             if(userVideoRef.current) {
+    //                 userVideoRef.current.srcObject = media;
+    //             }
+    //         } catch (error) {
+    //             console.log('Error accessing media devices', error)
+    //         }
+    //     }
+
+    //     initializeMedia();
+    // }, []);
+
+    // return (
+    //     <>
+    //         <div className='flex flex-col items-start justify-center min-h-screen bg-gray-700 ml-5'>
+    //             <h1 className='text-3xl font-bold mb-3 text-gray-900'>Live Video Streaming</h1>
+    //             <div className='relative w-1/4 max-w-3xl aspect-w-16 aspect-h-9 mb-4'>
+    //                 <video 
+    //                     ref={userVideoRef}
+    //                     autoPlay
+    //                     muted
+    //                     playsInline
+    //                     className='w-full h-full rounded-lg border border-gray-300 shadow-md mb-1'
+    //                 />
+    //                 <button
+    //                     onClick={handleStartStreaming}
+    //                     className='px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300'
+    //                 >
+    //                     Start Streaming
+    //                 </button>
+    //             </div>
+    //         </div>
+    //     </>
+    // )
+// }
+
+// export default Streamer;
