ff84139c89cc1ee0f03744a37a5ba7df593293ad
Billu <acharyaanshuman04@gmail.com>
2025-01-23 19:28:45 +0530

    Added relative, pulling for token expiry functionality added, css for stream URL, streamKey to be added

--- src/components/Devices/DeviceContainer.tsx
+++ src/components/Devices/DeviceContainer.tsx
@@ -9,7 +9,7 @@ function DeviceContainer() {
         <div className="flex items-center justify-center my-10">
             <h1 className="text-white font-bold text-6xl items-center">Devices</h1>
         </div>
-        <div className="flex items-center py-0 px-10 ml-14 fixed my-10 w-full">
+        <div className="flex items-center py-0 px-10 ml-14 relative my-10 w-full">
             <RegisterDevice />
             <DeviceList />
         </div>
